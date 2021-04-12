888cacfe6b86da57853b787c2d1b16f027c8659c
trungcaot <trungcaot@hotmail.com>
2021-04-12 08:24:04 +0700

    added material-ui in this project.

--- src/contants/appContant.ts
+++ src/contants/appContant.ts
@@ -1,1 +1,18 @@
+// APP TEXT 
+export const APP_TEXT = "Welcome, Developer";
+export const FOOTER_TEXT = `${new Date().getFullYear()} Built with ♡ by Welcome, Developer`;
+
+// PAGE TITLES
+export const PAGE_TITLE_HOME = "Home"
+export const PAGE_TITLE_DASHBOARD = "Dashboard"
+export const PAGE_TITLE_GH_PRIVATE = "GitHub Private"
+export const PAGE_TITLE_GH_PUBLIC = "GitHub Public"
+export const PAGE_TITLE_CODE = "Code Editor"
+export const PAGE_TITLE_SETTINGS = "Settings"
+
+// UI CONSTANTS
+export const FOOTER_HEIGHT = 30
+export const HEADER_HEIGHT = 60
+export const DRAWER_WIDTH = 250
+
 export const API_URL = 'https://your/api/url.com';
