8e332d7bb3accc1f399a650ad1a4270fb721caa8
trungcaot <trungcaot@hotmail.com>
2021-04-15 08:43:35 +0700

    removed partials folder.

--- src/theme/appTheme.ts
+++ src/theme/appTheme.ts
@@ -1,28 +1,27 @@
-import { createMuiTheme, Theme } from '@material-ui/core/styles';
+import { createMuiTheme, Theme } from "@material-ui/core/styles";
 
 // define light theme colors
 export const lightTheme: Theme = createMuiTheme({
-    palette: {
-        type: "light",
-        primary: {
-            main: '#115293',
-        },
-        secondary: {
-            main: '#1976d2',
-        },
-        
+  palette: {
+    type: "light",
+    primary: {
+      main: "#115293"
     },
+    secondary: {
+      main: "#1976d2"
+    }
+  }
 });
 
 // define dark theme colors
 export const darkTheme: Theme = createMuiTheme({
-    palette: {
-        type: "dark",
-        primary: {
-            main: '#388e3c',
-        },
-        secondary: {
-            main: '#4caf50',
-        },
+  palette: {
+    type: "dark",
+    primary: {
+      main: "#388e3c"
     },
+    secondary: {
+      main: "#4caf50"
+    }
+  }
 });
