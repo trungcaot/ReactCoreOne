0457453ee1fa868655006bc07372dd0aee606f72
trungcaot <trungcaot@hotmail.com>
2021-04-19 08:42:55 +0700

    preparing HomePage for testing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactElement } from "react";
+import React, { FC, ReactElement, useState } from "react";
 import { Helmet } from "react-helmet";
 import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 
@@ -16,8 +16,30 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
+const descriptions = [
+  {
+    title: "Test 1",
+    shortened: "Short description of Test 1",
+    full: "Full description of Test 1"
+  },
+  {
+    title: "Test 2",
+    shortened: "Short description of Test 2",
+    full: "Full description of Test 2"
+  }
+];
+
 const Home: FC<{}> = (): ReactElement => {
   const classes = useStyles();
+  const [showFullDescriptions, setShowFullDescriptions] = useState([...descriptions].map(() => false));
+
+  const handleReadMore = (index: number): void => {
+    const nextShowFullDescriptions = showFullDescriptions.map((prevValue, prevIndex) =>
+      prevIndex === index ? true : prevValue
+    );
+    setShowFullDescriptions(nextShowFullDescriptions);
+  };
+
   return (
     <>
       <Helmet>
@@ -27,6 +49,23 @@ const Home: FC<{}> = (): ReactElement => {
       </Helmet>
       <div className={classes.root}>
         <PageTitle title={PAGE_TITLE_HOME} />
+      </div>
+      <div>
+        {descriptions.map((description, index) => (
+          <div key={description.title}>
+            <h2>{description.title}</h2>
+            <h4>
+              {description.shortened}
+              {showFullDescriptions[index] ? (
+                <span>{description.full}</span>
+              ) : (
+                <button type="button" onClick={() => handleReadMore(index)}>
+                  Read more...
+                </button>
+              )}
+            </h4>
+          </div>
+        ))}
       </div>
     </>
   );
